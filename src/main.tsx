59e8871e5c6817330f74ec966317912022f52fe1
parksil0 <sirisiri0809@gmail.com>
2022-02-22 08:43:35 +0900

    feat : setting global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Global } from "@emotion/react";
+import normalize from "emotion-normalize";
+
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={normalize} />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
