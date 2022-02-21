0d28c84a2b40b028f26afdb648c9356fb5b72c27
parksil0 <sirisiri0809@gmail.com>
2022-02-22 08:42:06 +0900

    feat : keyboard ui 생성

---
+++ src/components/Keyboard/index.styles.ts
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import Button from "../Button";
+
+export const Grid = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  max-width: 700px;
+  height: 200px;
+  padding: 0px 10px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+export const KeyboardButton = styled(Button) <{ flex?: 1 | 1.5 }>`
+display: flex;
+  flex: ${({ flex }) => (flex ? `${flex}` : `1`)};
+  justify-content: center;
+  align-items: center;
+
+  background-color: #d3d6da;
+  font-weight: bold;
+  height: 58px;
+  border-radius: 4px;
+  text-transform: uppercase;
+  padding: 0px;
+`;
+
+export const KeyboardEmptySpace = styled.div`
+  flex: 0.5;
+`;
---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const keyboardArray = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "(X)"],
+];
+
+export const THIRD_ENTER_BUTTON_INDEX = 0;
+export const THIRD_DELETE_BUTTON_INDEX = 8;
+
+export const ERROR_MESSAGE = {
+  TEST: '테스트입니다.',
+}
