afcd7a0515819497f0322aa5f15fdd42c8aca713
parksil0 <sirisiri0809@gmail.com>
2022-02-22 08:42:32 +0900

    feat : 경고 메세지 ui 생성

---
+++ src/components/Alert/index.style.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const AlertWithoutButton = styled.p`
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: black;
+  color: white;
+  padding: 20px;
+  border-radius: 8px;
+  font-size: 1rem;
+  font-weight: bold;
+  animation: fade 2s linear;
+
+  @keyframes fade {
+    0% { opacity: 0 }
+    50% { opacity: 1 }
+    100% { opacity: 0 }
+  }
+}
+`;
