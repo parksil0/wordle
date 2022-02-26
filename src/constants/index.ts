4028ccad16bcd8719c47197f3f6a262273a6b793
parksil0 <sirisiri0809@gmail.com>
2022-02-26 16:48:40 +0900

    chore : 에러메세지 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,5 +8,6 @@ export const THIRD_ENTER_BUTTON_INDEX = 0;
 export const THIRD_DELETE_BUTTON_INDEX = 8;
 
 export const ERROR_MESSAGE = {
-  TEST: '테스트입니다.',
+  NOT_ENOUGH_LETTERS: 'Not enough letters',
+  NOT_IN_WORD_LIST: 'Not in word list',
 };
